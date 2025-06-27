abe944d3c3bafb6e123422731b98ef4299a6bcfd
ilnegro <maxbolo@maxbolo.com>
2025-06-27 10:07:49 +0200

    Aggiunta la directory steem-keychain-app con tutti i file

---
+++ steem-keychain-app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.steemkeychain.app', 
+  appName: 'Steem Keychain App',
+  webDir: 'web'
+};
+
+export default config;
